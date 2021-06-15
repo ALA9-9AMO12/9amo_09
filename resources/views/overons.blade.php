aa9c4409f2eb177f055b8c55816eef3e295f05db
Noah Bosch <61787786+noahbosch@users.noreply.github.com>
2021-06-15 10:01:52 +0200

    Add files via upload

--- resources/views/overons.blade.php
+++ resources/views/overons.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
     <!-- Styles -->
     <style>
@@ -18,7 +19,7 @@
         }
 
         main {
-            padding-top: 60px;
+            margin-top: 230px;
             padding-left: 15%;
             padding-right: 15%;
         }
@@ -52,18 +53,6 @@
             float: left;
         }
 
-        /* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
-        @media screen and (max-width: 500px) {
-            .header a {
-                float: none;
-                display: block;
-                text-align: left;
-            }
-            .header-left {
-                float: none;
-            }
-        }
-
         .subheaders-h2 {
             text-align: center;
         }
@@ -84,8 +73,10 @@
             background-color: antiquewhite;
         }
 
-        .table-msg {
+        .table {
             margin-top: 20%;
+            padding-right: 50px;
+            padding-left: 20px;
         }
 
         footer {
@@ -93,8 +84,20 @@
             background-color: lightgray;
             padding: 60px;
             margin: -20px;
-            margin-top: 30px;
+            margin-top: 550px;
         }
+
+        /* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
+        @media screen and (max-width: 500px) {
+            .header a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+            .header-left {
+                float: none;
+            }
+
     </style>
 </head>
 <body>
@@ -114,59 +117,35 @@
 </header>
 <main>
     <div class="text-right">
-        <div class="table-msg">
-            <p>MEEST RECENTE BERICHTEN</p>
-            <p>Bericht 1</p>
-            <p>――――――――</p>
-            <p>Bericht 2</p>
-            <p>――――――――</p>
-            <p>Bericht 3</p>
-            <p>――――――――</p>
-            <p>Bericht 4</p>
-            <p>――――――――</p>
-            <p>Bericht 5</p>
-        </div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="row">0</th>
+                <th>Bericht naam</th>
+                <th>Bericht</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Bericht 1</td>
+                <td>Bericht 1</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Bericht 2</td>
+                <td>Bericht 2</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
     <div class="text-left">
         <h2 class="subheaders-h2">OVER ONS</h2>
-        <p>De imkervereniging Oegstgeest en omstreken strekt zich uit over de regio Oegstgeest, Leiden, Lisse, Leimuiden
-            en Alkemade. De imkervereniging stelt zich ten doel de kennis over het houden van bijen en de relatie van
-            bijen met hun (planten) omgeving te verbreden bij zowel de imkers, als bij het grotere publiek.
-        </p>
-
-        <br>
-        <h2 class="subheaders-h2">LIDMAATSCHAP</h2>
         <p>
-            De imkervereniging-Oegstgeest is aangesloten bij de landelijke Nederlandse Bijenhoudersvereniging. Met een
-            lidmaatschap van de NBV bent u automatisch lid van één van de regio afdelingen. De NBV biedt u:
-        </p>
-        <p> - Belangenbehartiging van de imkerij in Nederland </p>
-
-        <p>    - Uitvoerig bijenteeltonderwijs programma </p>
-
-        <p>    - Zes keer per jaar ontvangst van het magazine BIJENhouden </p>
-
-        <p>    - Maandelijks het digitale Imkernieuws; </p>
-
-        <p>    - Wekelijks tips over het houden van bijen in het BijenBlog </p>
-
-        <p>    - Korting op entree voor bijeenkomsten en activiteiten met de NBV ledenpas; </p>
-
-        <p>    - Aanvullende <a href="https://www.bijenhouders.nl/over-de-nbv/verzekeringen">WA-verzekering</a> voor activiteiten die voortvloeien uit het houden van bijen; </p>
-
-        <p>    - Voorlichtingsmateriaal t.b.v presentaties </p>
-
-        <p>    - Verzekerd zijn van ondersteuning van collega imkers; </p>
-
-        <p>    - Voor al uw vragen kunt u zich wenden tot het <a href="https://www.bijenhouders.nl/over-de-nbv/lidmaatschap#aanmeldformulier">verenigingssecretariaat in Wageningen</a>. </p>
-        <br>
-        <p>
-            Informatie over contributie en een mogelijkheid tot aanmelding vindt u op de NBV website.
-        </p>
-        <br>
-        <p>
-            U kunt ook alleen donateur van de Oegstgeester Imkervereniging worden. Dat kunt u aanvragen via het
-            contactformulier.
+            Wij zijn een bijen en hommel imkervereniging rond de Oegstgeest omgeving, De imkervereniging Oegstgeest
+            en omstreken strekt zich uit over de regio Oegstgeest, Leiden, Lisse, Leimuiden en Alkemade. De
+            imkervereniging stelt zich ten doel de kennis over het houden van bijen en de relatie van bijen met hun
+            (planten) omgeving te verbreden bij zowel de imkers, als bij het grotere publiek.
         </p>
     </div>
 </main>
@@ -175,4 +154,3 @@
 </footer>
 </body>
 </html>
-
